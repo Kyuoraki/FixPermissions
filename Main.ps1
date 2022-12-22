a7f6f877cb7cde6a4a67bf1fd9517b87b6bd3b95
Kyuoraki <black_cloak56@mail.ru>
2022-12-22 17:15:24 +0500

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -9,7 +9,7 @@ foreach ($computer in  $hostsList) {
     $filter = 'Address="{0}" and Timeout={1}' -f $computer, 500 ## $computer - Hostname, 500 - ping timeout
     $availabilityCheck = Get-CimInstance -ClassName Win32_PingStatus -Filter $Filter #check online/ping
     if ( ($availabilityCheck.StatusCode) -eq 0 ) {
-        ssh n.bobrov@$computer -p 33 powershell.exe -command "hostname;powershell -file 'C:\Program Files\OpenSSH\FixHostFilePermissions.ps1';powershell -file 'C:\Program Files\OpenSSH\FixUserFilePermissions.ps1'" | Out-File "C:\Fix\log.txt" -Force
+        ssh n.bobrov@$computer -p 33 powershell.exe -command "hostname;powershell -file 'C:\Program Files\OpenSSH\FixHostFilePermissions.ps1';powershell -file 'C:\Program Files\OpenSSH\FixUserFilePermissions.ps1'" | Out-File "C:\Fix\log.txt" -Force -Append
     }else{
         $foreach.MoveNext() | Out-Null
     }
